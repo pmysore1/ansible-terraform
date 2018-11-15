147c246eac0f61bc2f2b1fcbfe7a5f8cf01f21d8
Mysore <pradeep.mysore@capgemini-gs.com>
2018-11-14 19:54:58 -0500

    updated changes

--- ec2/main.tf
+++ ec2/main.tf
@@ -15,5 +15,5 @@ data "aws_availability_zones" "zones" {}
 
 
 
-#terraform init -backend-config="bucket=poc-cog-tfstate" -backend-config="key=ec2/ec2.tfstate" -backend-config="region=us-gov-west-1" -backend-config="profile=cognosante"
-#terraform remote config -backend=S3 -backend-config="bucket=terraform-poc-state" -backend-config="key=vpc/vpc.tfstate" -backend-config="region=us-gov-west-1"
+
+
