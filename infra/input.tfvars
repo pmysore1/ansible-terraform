233cd687c7486141bc2ff01ef359aeed9ac03760
Mysore <pradeep.mysore@capgemini-gs.com>
2018-11-21 09:12:19 -0500

    Updates

--- infra/input.tfvars
+++ infra/input.tfvars
@@ -13,3 +13,13 @@ baseos_ec2_ami_id = "ami-906cf0f1"
 ec2_instance_type = "t2.micro"
 aws_ec2_user        = "ec2-user"
 vpc_keypair         = "poc-anisble-tower"
+
+s3_pem_file_path        = "s3://poc-cog-tfstate/auth"
+s3_pem_file_name        = "poc-anisble-tower-cognosante.pem"
+
+#following values for controlling access to AWD resources
+ssh_remote_cidr_block = "173.67.205.219/32,66.102.238.226/32"
+https_remote_cidr_block = "173.67.205.219/32,66.102.238.226/32"
+rdp_remote_cidr_block = "173.67.205.219/32,66.102.238.226/32"
+winrm_remote_cidr_block = "173.67.205.219/32,66.102.238.226/32"
+vpn_remote_cidr_block  = "173.67.205.219/32,66.102.238.226/32"
--- infra/input_variables.tf
+++ infra/input_variables.tf
@@ -1,60 +1,22 @@
-"variable" "remote-state-bucket_name" {}
-"variable" "terraform-remote-state-vpc-key" {}
-"variable" "aws_region" {}
-"variable" "aws_profile" {}
-"variable" "environment" {}
+variable "remote-state-bucket_name" {}
+variable "terraform-remote-state-vpc-key" {}
+variable "aws_region" {}
+variable "aws_profile" {}
+variable "environment" {}
+variable "name-prefix" {}
+variable "aws_ec2_user" {}
 
-"variable" "public-az3-cidr" {
-  "type" = "string"
-  "default" = "10.0.2.0/24"
-}
-"variable" "database-az1-cidr" {
-  "type" = "string"
-  "default" = "10.0.8.0/24"
-}
-"variable" "unique_id" {
-  "type" = "string"
-  "default" = "ffd025"
-}
-"variable" "private-az2-cidr" {
-  "type" = "string"
-  "default" = "10.0.5.0/24"
-}
-"variable" "database-az2-cidr" {
-  "type" = "string"
-  "default" = "10.0.9.0/24"
-}
-"variable" "name-prefix" {}
+variable "vpc_keypair" {}
 
-"variable" "public-az1-cidr" {
-  "type" = "string"
-  "default" = "10.0.0.0/24"
-}
-
-"variable" "private-az1-cidr" {
-  "type" = "string"
-  "default" = "10.0.4.0/24"
-}
-"variable" "public-az2-cidr" {
-  "type" = "string"
-  "default" = "10.0.1.0/24"
-}
-"variable" "dhcp-domain-name" {
+variable "dhcp-domain-name" {
   "type" = "string"
   "default" = "us-gov-west-1.compute.internal"
 }
-"variable" "database-az3-cidr" {
-  "type" = "string"
-  "default" = "10.0.10.0/24"
-}
-"variable" "vpc_cidr_block" {
+variable "vpc_cidr_block" {
   "type" = "string"
   "default" = "10.0.0.0/20"
 }
-"variable" "private-az3-cidr" {
-  "type" = "string"
-  "default" = "10.0.6.0/24"
-}
+
 variable "max_subnet_count" {
   default     = 0
   description = "Sets the maximum amount of subnets to deploy.  0 will deploy a subnet for every availablility zone within the region"
@@ -112,26 +74,25 @@ variable "dns_support" {
 
 # Remote access
 variable "ssh_remote_cidr_block" {
-  default = "173.67.205.219/32,66.102.238.226/32"
+  default = ""
 }
 
 variable "https_remote_cidr_block" {
-  default = "173.67.205.219/32,66.102.238.226/32"
+  default = ""
 }
 
 variable "rdp_remote_cidr_block" {
-  default = "173.67.205.219/32,66.102.238.226/32"
+  default = ""
 }
 
 variable "winrm_remote_cidr_block" {
-  default = "173.67.205.219/32,66.102.238.226/32"
+  default = ""
 }
 
 variable "vpn_remote_cidr_block" {
-  default = "173.67.205.219/32,66.102.238.226/32"
+  default = ""
 }
 
-variable "vpc_keypair" {}
 
 # Base AMI to use for hardened RHEL7 instances
 variable "baseos_rhel7_ami_id" {
@@ -142,10 +103,5 @@ variable "baseos_ec2_ami_id" {}
   
 variable "ec2_instance_type" {}
 
-variable "s3_pem_file_path" {
-  default = "s3://poc-cog-tfstate/auth"
-}
-variable "s3_pem_file_name" {
-  default = "poc-anisble-tower-cognosante.pem"
-}
-variable "aws_ec2_user" {}
+variable "s3_pem_file_path" {}
+variable "s3_pem_file_name" {}
